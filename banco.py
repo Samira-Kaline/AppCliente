2c37a0314871258e89714b8fc468d8237e96d6a3
maxandrade7 <maxandrade909@gmail.com>
2018-10-17 15:29:12 -0300

    Correção no auto-incremento das tabelas

--- banco.py
+++ banco.py
@@ -8,7 +8,7 @@ cursor = conn.cursor()
 # criando a tabela de campus.
 cursor.execute("""
 CREATE TABLE tb_campus (
-        id_campus INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        id_campus INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
         nome_campus TEXT NOT NULL,
         cidade TEXT NOT NULL,
         sigla VARCHAR(3) NOT NULL
@@ -18,7 +18,7 @@ CREATE TABLE tb_campus (
 # criando a tabela de aluno.
 cursor.execute("""
 CREATE TABLE tb_aluno (
-        id_aluno INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        id_aluno INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
         matricula_aluno VARCHAR(12) NOT NULL UNIQUE,
         nome TEXT NOT NULL,
         nascimento DATE NOT NULL,
@@ -32,7 +32,7 @@ CREATE TABLE tb_aluno (
 # criando a tabela de nutricionista.
 cursor.execute("""
 CREATE TABLE tb_nutricionista (
-        id_nutricionista INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        id_nutricionista INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
         nome TEXT NOT NULL,
         siape VARCHAR(10) NOT NULL UNIQUE,
         CRN TEXT NOT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE tb_nutricionista (
 # criando a tabela de refeição.
 cursor.execute("""
 CREATE TABLE tb_refeicao (
-        id_refeicao INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        id_refeicao INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
         nome TEXT NOT NULL,
         hr_inicial TIME NOT NULL,
         hr_final TIME NOT NULL,
